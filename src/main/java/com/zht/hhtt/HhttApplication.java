eba5d3ff15a6bd996f3e1237c1244ccd5c590e3a
13249723979 <1093315619@qq.com>
2020-03-30 16:22:01 +0800

    Initial commit

---
+++ src/main/java/com/zht/hhtt/HhttApplication.java
@@ -0,0 +1,10 @@
+package com.zht.hhtt;
+
+
+public class HhttApplication {
+
+    public static void main(String[] args) {
+        //首次提交
+    }
+
+}
---
+++ src/test/java/com/zht/hhtt/HhttApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zht.hhtt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhttApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
